0454e8c21cf5e82ba3245b73440d7f4421477066
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-03-29 07:50:52 +0200

    git: init commit

---
+++ src/01/index.js
@@ -0,0 +1,25 @@
+const elBox = document.querySelector('#box');
+
+// Pure function that returns the next state,
+// given the current state and sent event
+function transition(state, event) {
+  switch (
+    state
+    // Add your state/event transitions here
+    // to determine and return the next state
+  ) {
+  }
+}
+
+// Keep track of your current state
+let currentState = undefined;
+
+function send(event) {
+  // Determine the next value of `currentState`
+
+  elBox.dataset.state = currentState;
+}
+
+elBox.addEventListener('click', () => {
+  // send a click event
+});
