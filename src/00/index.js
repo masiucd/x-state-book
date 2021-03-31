95c6545efa1327b99062ca0a60a8c2e0b9e0dabb
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-03-31 07:21:38 +0200

    feat: vanilla state machines

--- src/00/index.js
+++ src/00/index.js
@@ -1,17 +1,48 @@
-import { createMachine } from 'xstate';
+import { createMachine } from "xstate";
 
-const elOutput = document.querySelector('#output');
+const elOutput = document.querySelector("#output");
 
 function output(object) {
   elOutput.innerHTML = JSON.stringify(object, null, 2);
 }
 
-console.log('Welcome to the XState workshop!');
-
 const user = {
-  name: 'David Khourshid',
-  company: 'Microsoft',
-  interests: ['piano', 'state machines'],
+  name: "masiu",
+  company: "etraveli",
+  interests: ["piano", "state machines"],
 };
 
 output(user);
+
+const machine = {
+  initial: "idle",
+  states: {
+    idle: {
+      on: {
+        FETCH: "pending",
+      },
+    },
+    pending: {
+      on: {
+        RESOLVE: "resolved",
+        REJECT: "rejected",
+      },
+    },
+    resolved: {},
+    rejected: {},
+  },
+};
+
+const transition = (state, event) => {
+  return machine.states[state]?.on?.[event] || state;
+};
+
+output(transition("idle", "FETCH"));
+let currentState = machine.initial;
+
+const send = event => {
+  const nextState = transition(currentState, event);
+  currentState = nextState;
+};
+
+window.send = send;
--- src/01/index.js
+++ src/01/index.js
@@ -1,25 +1,59 @@
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 // Pure function that returns the next state,
 // given the current state and sent event
-function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
-  }
-}
+// function transition(state, event) {
+// switch (state) {
+//   case "inactive":
+//     switch (event) {
+//       case "CLICK":
+//         return "active";
+
+//       default:
+//         return state;
+//     }
+//   case "active":
+//     switch (event) {
+//       case "CLICK":
+//         return "inactive";
+//       default:
+//         return state;
+//     }
+//   default:
+//     return state;  // }
+// }
+
+const machine = {
+  initial: "idle",
+  states: {
+    inactive: {
+      on: {
+        CLICK: "active",
+      },
+    },
+    active: {
+      on: {
+        CLICK: "inactive",
+      },
+    },
+  },
+};
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = "inactive";
+
+const transition = (state, event) => {
+  const nextState = machine.states[state].on?.[event] || state;
+  return nextState;
+};
 
 function send(event) {
-  // Determine the next value of `currentState`
+  currentState = transition(currentState, event);
 
+  console.log(currentState);
   elBox.dataset.state = currentState;
 }
 
-elBox.addEventListener('click', () => {
-  // send a click event
+elBox.addEventListener("click", () => {
+  send("CLICK");
 });
