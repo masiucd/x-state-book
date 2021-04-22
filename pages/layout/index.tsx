5452ac9b534bbd76db44e090fcf0acb0aa73edff
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-04-22 08:06:37 +0200

    feat: layout component

---
+++ pages/layout/index.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+import React from "react";
+
+interface LayoutProps {
+  fluid?: boolean;
+}
+const Page = styled.main<LayoutProps>`
+  max-width: ${porps => (porps.fluid ? "100%" : "900px")};
+`;
+
+const Layout: React.FC<LayoutProps> = ({ children, fluid }) => {
+  return (
+    <>
+      <Page fluid={fluid}>{children}</Page>
+    </>
+  );
+};
+
+export default Layout;
