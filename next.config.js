f51c63104022748b3ee1d1d253eaada50afd1d53
masiucd <ciszekmarcell@gmail.com>
2023-02-18 19:14:48 +0100

    fix: remove unused package

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const {withContentlayer} = require("next-contentlayer")
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -7,4 +5,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withContentlayer(nextConfig)
+module.exports = nextConfig
