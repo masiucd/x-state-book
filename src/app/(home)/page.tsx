e7a3596c19352fc0170fcf958b7af318303dfddf
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-03-08 14:02:06 +0100

    new start page + reset of nav and header

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,25 +1,22 @@
 import * as Icon from "react-feather"
 
-import Button from "@/components/common/button"
-
 const cards = [
   {
     title: "Handle complex logic",
     description:
-      "XState is a JavaScript library for modeling and managing state",
+      "Make complex logic easy to understand and maintain with statecharts",
     image: Icon.BarChart,
   },
   {
     title: "State machines and statecharts",
     description:
-      "XState is a JavaScript library for modeling and managing state",
-    image: Icon.BarChart,
+      "Statecharts are a formal notation for describing the behavior of state machines",
+    image: Icon.Activity,
   },
   {
-    title: "Simplify complex logic",
-    description:
-      "XState is a JavaScript library for modeling and managing state",
-    image: Icon.Anchor,
+    title: "Awesome Developer experience",
+    description: "Great developer experience with X state and the devtools",
+    image: Icon.Code,
   },
   {
     title: "Finite State Machines",
@@ -27,55 +24,30 @@ const cards = [
       "Finite State Machines are a mathematical model of computation that can be used to describe any system with discrete states",
     image: Icon.Award,
   },
-  {
-    title: "Statecharts",
-    description:
-      "Statecharts are a formal notation for describing the behavior of state machines",
-    image: Icon.Bell,
-  },
-  {
-    title: "Actor model",
-    description:
-      "The actor model is a mathematical model of concurrent computation",
-    image: Icon.BellOff,
-  },
 ]
 
 export default function Home() {
   return (
-    <div className="flex-1 items-center justify-center py-20">
-      <div className="mb-10 grid grid-cols-1 gap-5 lg:grid-cols-6">
-        <div className="col-span-3 mb-5 lg:mb-0 ">
-          <h1 className="block bg-gradient-to-b from-slate-900 to-slate-400 bg-clip-text text-transparent drop-shadow-md">
-            X-State
-          </h1>
-          <h2 className="text-4xl">state machines and state charts!</h2>
-          <p>Simplify complex logic and state management with state-charts.</p>
-          <div className="flex gap-5">
-            <Button apperence="primary" href="/machines">
-              Get Started
-            </Button>
-            <Button href="/about">Why XState?</Button>
-            <Button href="/docs">Docs</Button>
-          </div>
-        </div>
-        <div>
-          {/* TODO own logo */}
-          {/* <Icons.xstate /> */}
-        </div>
+    <div className="flex flex-1 flex-col items-center justify-center py-20">
+      <div className="mb-20">
+        <h1 className="border-b-2 border-slate-500">
+          <span className="text-4xl font-bold md:text-6xl">X state book</span>
+        </h1>
       </div>
-
-      <ul className="grid grid-cols-1 gap-5 lg:grid-cols-3">
+      <ul className="grid grid-cols-2 gap-5 p-5">
         {cards.map((card) => (
-          <li
-            key={card.title}
-            className="flex flex-col gap-5 rounded-md border-2 border-slate-900 bg-slate-50 p-2 shadow"
-          >
+          <li key={card.title}>
             <div>
-              <card.image />
+              <div>
+                <card.image />
+              </div>
+              <div>
+                <h3 className="text-2xl font-bold">{card.title}</h3>
+                <p className="text-gray-500 dark:text-gray-400">
+                  {card.description}
+                </p>
+              </div>
             </div>
-            <h3>{card.title}</h3>
-            <p className="mt-auto">{card.description}</p>
           </li>
         ))}
       </ul>
