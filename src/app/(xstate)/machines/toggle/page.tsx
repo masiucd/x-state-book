5b10afc628a3f40bdd322a0faa3f2731591caf20
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2023-03-10 18:08:09 +0100

    feat: new package lock

--- src/app/(xstate)/machines/toggle/page.tsx
+++ src/app/(xstate)/machines/toggle/page.tsx
@@ -1,5 +1,3 @@
-import ToggleApp from "@/components/xstate/toggle/toggle_app"
-
 import ToggleHighlighter from "./toggle_highlighter"
 
 export default function ToggleMachinePage() {
@@ -7,7 +5,6 @@ export default function ToggleMachinePage() {
     <div className="flex flex-col gap-5">
       <h1>Toggle App</h1>
       <div className="px-3">
-        <ToggleApp />
         <ToggleHighlighter />
       </div>
     </div>
--- src/lib/utils/helper.ts
+++
@@ -1,3 +0,0 @@
-export function isOnServer() {
-  return typeof window === "undefined"
-}
